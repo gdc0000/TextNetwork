8ed4512414ce52edee0fe00a574c60787d1e170b
Alfiocan000 <gabriele.dicicco@gmail.com>
2026-06-22 01:12:12 +0200

    remove personal footer from app

--- src/app.py
+++ src/app.py
@@ -189,11 +189,4 @@ def main() -> None:
     else:
         st.info("Preprocess your data to enable topic modeling.")
 
-    # Footer
-    st.markdown("---")
-    st.markdown("### **Gabriele Di Cicco, PhD in Social Psychology**")
-    st.markdown(
-        "[GitHub](https://github.com/gdc0000) | "
-        "[ORCID](https://orcid.org/0000-0002-1439-5790) | "
-        "[LinkedIn](https://www.linkedin.com/in/gabriele-di-cicco-124067b0/)"
-    )
+
